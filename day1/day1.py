7053791bed7dd841c3033bf10da363ebf1ce4760
Corentin Chataignon <corentin.chataignon44@gmail.com>
2022-12-01 09:24:43 +0100

    feat(day1): add day1

---
+++ day1/day1.py
@@ -0,0 +1,14 @@
+with open("input.txt") as f:
+    calories = {}
+    cur_elf = 0
+
+    for l in f.readlines():
+        l = l.strip()
+
+        if l != "":
+            calories[cur_elf] = calories.get(cur_elf, 0) + int(l)
+        else:
+            cur_elf += 1
+
+print(max(calories.values()))
+print(sum(sorted(calories.values(), reverse=True)[:3]))
