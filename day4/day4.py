8dd210f1a5329e0ea8f94d818d99f5aeade5e256
Corentin Chataignon <corentin.chataignon44@gmail.com>
2022-12-04 15:55:02 +0100

    feat(day4): add day4

---
+++ day4/day4.py
@@ -0,0 +1,22 @@
+n_overlap = 0
+n_overlap_p2 = 0
+
+with open("input.txt") as f:
+
+    for l in f.read().splitlines():
+        section = l.split(",")
+
+        s1_min, s1_max = map(int, section[0].split("-"))
+        s2_min, s2_max = map(int, section[1].split("-"))
+
+        if (s1_min <= s2_min and s1_max >= s2_max) or (
+            s2_min <= s1_min and s2_max >= s1_max
+        ):
+            n_overlap += 1
+
+        if len(set(range(s1_min, s1_max + 1)) & set(range(s2_min, s2_max + 1))):
+            n_overlap_p2 += 1
+
+
+print(n_overlap)
+print(n_overlap_p2)
